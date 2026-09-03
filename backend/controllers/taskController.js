1619af7c124bfce8ef7ef04d44ffe30454b9a345
IT24104083 <137370525+thamidu02@users.noreply.github.com>
2026-09-03 22:52:59 +0530

    created the delete task api

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -18,3 +18,24 @@ export const createTask = async (req, res) => {
     });
   }
 };
+
+export const deleteTask = async (req, res) => {
+  try {
+    const task = await Task.findByIdAndDelete(req.params.id);
+
+    if (!task) {
+      return res.status(404).json({
+        message: "Task not found",
+      });
+    }
+
+    res.status(200).json({
+      message: "Task deleted successfully",
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Failed to delete task",
+      error: error.message,
+    });
+  }
+};
