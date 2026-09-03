90e5d676a58ef09910753ed150bd146620213c5d
dasunithiwanshika-ship-it <dasunithiwanshikagmail.com>
2026-09-03 21:29:25 +0530

    Add update note API

--- backend/routes/noteRoutes.js
+++ backend/routes/noteRoutes.js
@@ -1,9 +1,14 @@
 import express from "express";
-import { createNote, getNotes } from "../controllers/noteController.js";
+import {
+  createNote,
+  getNotes,
+  updateNote,
+} from "../controllers/noteController.js";
 
 const router = express.Router();
 
 router.post("/", createNote);
 router.get("/", getNotes);
+router.put("/:id", updateNote);
 
 export default router;
